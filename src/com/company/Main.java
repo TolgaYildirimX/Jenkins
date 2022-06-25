a4fa048cbb4bed6b00451e849078a2eef8e2753c
tolga <tolgayyildirim87@gmail.com>
2022-06-25 12:52:15 -0500

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         for(int i = 0; i<5; i++) {
             System.out.println("hello am i tolga ?");
+            System.out.println("yes i am tolga");
         }
     }
 }
